e21e0e4ae1d4bfe11078282cf71d0cab4e2b3ebc
Ngan2k4 <thaongangithub@gmail.com>
2025-12-10 19:45:07 +0700

    KAN-147: Tạo layout và cấu trúc view cơ bản

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddDefaultTokenProviders()
     .AddDefaultUI()
