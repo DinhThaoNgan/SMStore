fed67b28ff3b55c9a35a68f1ebdacdfcb64feb33
kogtrung <ct34bgithub@gmail.com>
2026-01-04 19:38:26 +0700

    fix banner

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 using System.Diagnostics;
 using CuaHangBanSach.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CuaHangBanSach.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.Sliders = await _context.Sliders.ToListAsync();
             return View();
         }
 
